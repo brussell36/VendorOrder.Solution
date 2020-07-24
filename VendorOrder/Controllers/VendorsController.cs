329ead7d23d63c0bd89df0dc5c1b72902a123835
Ben Russell <benjaminrussell36@gmail.com>
2020-07-24 13:28:32 -0700

    Add route to Vendors Show.cshtml.

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -27,5 +27,16 @@ namespace VendorOrder.Controllers
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Order;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
   }
 }
