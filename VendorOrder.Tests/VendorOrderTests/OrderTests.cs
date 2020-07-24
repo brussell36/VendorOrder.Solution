aac7a668d69c684a4daf006b6cc77cbe3325d0ee
Ben Russell <benjaminrussell36@gmail.com>
2020-07-24 10:46:25 -0700

    Add test for Order object to check if it holds values. Fail test.

--- VendorOrder.Tests/VendorOrderTests/OrderTests.cs
+++ VendorOrder.Tests/VendorOrderTests/OrderTests.cs
@@ -17,10 +17,20 @@ namespace VendorOrder.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("test");
+      Order newOrder = new Order("test", "test", "test", "test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
-
+    [TestMethod]
+    public void GetValues_ReturnValues_String()
+    {
+      string title = "Ben's Bakery";
+      string description = "Wants 30 loaves";
+      string price = "$90";
+      string date = "Friday Jul 24";
+      Order newOrder = new Order(title, description, price, date);
+      string result = newOrder.Description;
+      Assert.AreEqual(5, result);
+    }
   }
 }
