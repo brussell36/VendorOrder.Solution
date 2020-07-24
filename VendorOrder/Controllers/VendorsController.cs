e49c4b05a47c57e8ae167ea1b794bd8881122b0c
Ben Russell <benjaminrussell36@gmail.com>
2020-07-24 11:00:10 -0700

    Add html to New, Show and Index for Vendors.

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -14,5 +14,11 @@ namespace VendorOrder.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
   }
 }
